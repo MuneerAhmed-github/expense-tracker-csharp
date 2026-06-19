d43a674a94c2385600d723ac8ed7882ec18e350a
muneerahmed0704@gmail.com <muneerahmed0704@gmail.com>
2026-06-19 21:11:51 +0530

    Splitted the Classes in to Seperate File For Clean Architecture

---
+++ Expense.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker
+{
+    class Expense
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public decimal Amount { get; set; }
+        public string? Category { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("ID             : " + Id);
+            Console.WriteLine("Title          : " + Title);
+            Console.WriteLine("Amount           : ₹" + Amount);
+            Console.WriteLine("Category          : " + Category);
+            Console.WriteLine("Date         :" + Date.ToString("dd-MM-yyyy"));
+            Console.WriteLine("---------------------------------");
+        }
+
+        public string ToFileString()
+        {
+            return Id + "|" + Title + "|" + Amount + "|" + Category + "|" + Date.ToString("dd-MM-yyyy");
+        }
+    }
+
+}
